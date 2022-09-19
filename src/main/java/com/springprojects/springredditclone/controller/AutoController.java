ab077ce43c134f28c367c944aebf80b0fba67512
Prince Mandal <43564268+prince7861@users.noreply.github.com>
2022-09-19 20:43:00 +0530

    Updated the controller
    made user registration success

--- src/main/java/com/springprojects/springredditclone/controller/AutoController.java
+++ src/main/java/com/springprojects/springredditclone/controller/AutoController.java
@@ -1,6 +1,10 @@
 package com.springprojects.springredditclone.controller;
 
 import com.springprojects.springredditclone.dto.RegisterRequest;
+import com.springprojects.springredditclone.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
+@AllArgsConstructor
 public class AutoController {
-
+   private final AuthService authService;
     @PostMapping("/signup")
-    public void signup(@RequestBody RegisterRequest registerRequest)
+    public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest)
     {
+    authService.signup(registerRequest);
+    return new ResponseEntity<>("User Registration Successful", HttpStatus.OK);
 
     }
 }
